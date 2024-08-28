66d26a6db6211980f39752361bbaa0b97b180254
losobka <lukasz.osobka@gmail.com>
2024-08-28 10:14:20 +0200

    refactor: rector config sets update

--- rector.php
+++ rector.php
@@ -6,8 +6,10 @@ use Rector\Config\RectorConfig;
 use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -20,6 +22,12 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_82,
         LevelSetList::UP_TO_PHP_83,
+        SetList::CODING_STYLE,
+        SetList::CODE_QUALITY,
+        SetList::PHP_82,
+        SetList::PHP_82,
+        SetList::PHP_83,
+        SetList::DEAD_CODE,
         SymfonySetList::SYMFONY_62,
         SymfonySetList::SYMFONY_63,
         SymfonySetList::SYMFONY_64,
@@ -27,6 +35,8 @@ return static function (RectorConfig $rectorConfig): void {
         SymfonySetList::SYMFONY_71,
         PHPUnitSetList::PHPUNIT_100,
     ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_83);
 
     $rectorConfig->rule(DeclareStrictTypesRector::class);
     $rectorConfig->skip([
